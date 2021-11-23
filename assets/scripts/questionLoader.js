61335da51608b56eaa2c6babdea6cbfba7ac77a8
Beta Pictoris <git@mrhallway.me>
2021-11-23 20:22:02 +0000

    Update questions

--- assets/scripts/questionLoader.js
+++ assets/scripts/questionLoader.js
@@ -4,7 +4,7 @@ var url = "/api/questions/questions.json";
 var xhr = new XMLHttpRequest();
 xhr.open("GET", url);
 
-var ansID = rando(1,17);
+var ansID = rando(1,18);
 
 xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
